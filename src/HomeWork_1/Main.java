1a3fdb07ace0bd42fd4f029dc574435ade803c93
AndreiKashko <andreiKashko.91@gmail.com>
2025-08-06 12:43:21 +0300

    initial commit

---
+++ src/HomeWork_1/Main.java
@@ -0,0 +1,47 @@
+package HomeWork_1;
+
+public class Main {
+
+    public static class Homework1 {
+        public static void main(String[] args) {
+
+            //1
+            int b = 10;
+            int c = 15;
+            int a = 4 * (b + c - 1) / 2;
+            System.out.println(a);
+        }
+
+
+        //2
+        //int a = 26;
+        //int b = a % 10;
+        //int c = a /10;
+        //int d = b +c;
+        //System.out.println(d);
+
+        //3
+        //int a = 126;
+        //int b = a / 100 ;
+        //int c = a / 10 % 10;
+        //int d = a % 100 % 10;
+        //int x = b + c + d;
+        //System.out.println(x);
+
+        //4
+        //Double n = 32.0;
+        //double a = (n + 0.5)/1 ;
+        //int b = (int) a;
+        //System.out.println(b);
+
+
+        //5
+        //int q =21;
+        //int w = 8 ;
+        //int a = q / w;
+        //int b = q % w;
+        //System.out.print(a);
+        //System.out.print(".");
+        //System.out.print(b);
+    }
+}
